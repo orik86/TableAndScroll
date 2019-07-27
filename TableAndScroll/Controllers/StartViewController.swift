76c2eac3f1413a1ade592b081eb86b29fc0f2cd4
Aleksandr <nummy@mail,ru>
2019-07-27 23:53:22 +0500

    finish

--- TableAndScroll/Controllers/StartViewController.swift
+++ TableAndScroll/Controllers/StartViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class StartViewController: UIViewController {
     
     
+    @IBOutlet var imageSk: UIImageView!
     @IBOutlet weak var numbersOfImage: UILabel!
     
     
@@ -21,10 +22,21 @@ class StartViewController: UIViewController {
         numbersOfImage.text = "1"
     }
     
+
     @IBAction func sliderChanged(_ sender: UISlider) {
         numbersOfImage.text = String(Int(round(sender.value)))
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            imageSk.isHidden = true
+            
+        } else {
+            imageSk.isHidden = false
+            
+        }
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "startSegue" {
             let tableView = segue.destination as? TableViewController
